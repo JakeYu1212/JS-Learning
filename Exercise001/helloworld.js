52085c3c8b5fa91a62d6c9747d0fae094ce0251e
Gen.Yu <yugen_1212@163.com>
2018-10-24 21:42:03 +0800

    @starring

--- Exercise001/helloworld.js
+++ Exercise001/helloworld.js
@@ -11,7 +11,43 @@ function createIncrementor(start) {
 
 var inc = createIncrementor(5);
 
+document.write("<h1>This is a heading</h1>");
+document.write("<p>This is a paragraph.</p>");
 
-alert(inc()) // 5
-alert(inc()) // 6
-alert(inc()) // 7
+function changeContent()
+{
+x=document.getElementById("demo");  // 找到元素
+x.innerHTML="Hello JavaScript!";    // 改变内容
+}
+
+function changeImage()
+{
+element=document.getElementById('myimage')
+if (element.src.match("eg_bulbon"))
+  {
+  element.src="/JS-Learning/Exercise001/eg_bulboff.gif";
+  }
+else
+  {
+  element.src="/JS-Learning/Exercise001/eg_bulbon.gif";
+  }
+}
+
+function changeStyle()
+{
+x=document.getElementById("demo") // 找到元素
+x.style.color="#ff0000";          // 改变样式
+}
+
+function isNumber()
+{
+var x=document.getElementById("input0").value;
+if(x==""||isNaN(x))
+	{
+	alert("Not Numeric");
+  }
+}
+
+//  alert(inc())
+//  alert(inc())
+//  alert(inc())
